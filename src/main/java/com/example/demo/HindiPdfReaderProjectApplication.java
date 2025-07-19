76fc821c62589fb2b90d1698d499ff54ffe3d6dc
poojanehraofficial <poojanehraofficial@gmail.com>
2025-07-19 15:04:19 +0530

    Initial commit of hindi pdf reader project

---
+++ src/main/java/com/example/demo/HindiPdfReaderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HindiPdfReaderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HindiPdfReaderProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/HindiPdfReaderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HindiPdfReaderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
